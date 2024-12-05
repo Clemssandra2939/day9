10017e6ac4c4a4dff5c3c127e0b80eb5071fa988
Clemssandra2939 <sclems600@gmail.com>
2024-12-05 05:45:46 +0100

    Added todo 1

--- GradingProgram.py
+++ GradingProgram.py
@@ -5,7 +5,26 @@
 # and their rades for values,the final version of the studrnt_grades dictionary will be checked.
 
 student_score = {
+"Harry":81,
+"Ron": 78,
+"Hermione":99,
+"Draco":74,
+"Neville":62,
+}
+
+# TODO-1- Create an empty dictionary called student_grades
+student_grades = {}
+
+# TODO-2- Write your code below to add the grades 
+# Scoring Citeria :
+# 91-100:Grade = "Outstanding"
+# 81 -90 :Grade =  "Exceeds Expectation"
+# 71-80 :Grade = "Acceptable"
+# 70 or lower:Grade = "Fail"
 
 
-    
- }
+
+
+
+
+print(student_grades)
