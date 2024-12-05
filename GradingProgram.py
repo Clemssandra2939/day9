c021b454b6ee46054c50f67652fed065c5e85bac
Clemssandra2939 <sclems600@gmail.com>
2024-12-05 05:17:22 +0100

    Adding instructions

---
+++ GradingProgram.py
@@ -0,0 +1,7 @@
+# Instructions
+# The keys in student_scores are the names of the students and value are their exam scores.
+# Write a program that convert their score to grades,By the end of the program,
+# you should have a new dictionary called student_grades tht should contain students  names for keys
+# and their rades for values,the final version of the studrnt_grades dictionary will be checked.
+
+student_score = { }
