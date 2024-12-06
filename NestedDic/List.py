a8055080cde669d9cc0c032e03757170f1597d04
Clemssandra2939 <sclems600@gmail.com>
2024-12-06 05:50:18 +0100

    Nesting a list in adictionary

---
+++ NestedDic/List.py
@@ -0,0 +1,27 @@
+# Nesting Lists and Dictionaries
+
+# {
+#     key:[list],
+#     key2:{Dict},
+# }
+
+# Using a list and a dictionary as a value nested on another dictionary{}
+
+# Nesting
+capitals ={
+ "France":"Paris",
+ "Germany":"Berlin",
+}
+
+# Nesting a list in a Dictionary
+travel_log ={
+    "France":["Paris","Lille","Dijon"], #here list is used to mention different state in france because a key should hae one value
+    "Germany":["Berlin","Hsmburg","stuttgart"],
+}
+
+# Nesting Dictionary in a Dictionary
+
+travel_log ={
+    "France":["Paris","Lille","Dijon"], #
+    "Germany":["Berlin","Hsmburg","stuttgart"],
+}
