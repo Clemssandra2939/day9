0735b85fcf968caf4fcffc51f370c145ffd9fc7a
Clemssandra2939 <sclems600@gmail.com>
2024-12-09 05:28:01 +0100

    Uploaded the play file

--- DictionaryinaList.py
+++ DictionaryinaList.py
@@ -1,17 +1,16 @@
-# Instructions
-# You are going to write a program that add to a travel_log,
-# You can see a travel_log which is a List that contain 2 dictionaries.
 
-# Write a function that will work the following line of code on line 21 to add the  entry for Russia to the travel_log.
-
-# add_new_country {"Russia",2,["Moscow","Saint Peterburg"]}
-
-# You 've visited Russia 2 times.
-# You 've  been to Moscow and Saint Peterburg 
-
-# Do Not modify the travel_log directly,You need to create  a functiom that odify it 
-
-# Hint
-# Look at the function call above to see what the nae of function should be.
-# the iput for the function are positional arguments.the order is very iportant.
-
+travel_log = [
+{
+    "country" :"France",
+    "total_visited": 12,
+    "cities_visited" : ["Paris","Lille","Dijon"],
+     
+}, 
+{
+    "country" :"Germany",
+    "total_visited": 5,
+    "cities_visited" :["Berlin","Hamburg","stuttgart"],
+        
+},
+]
+ 
---
+++ play.py
@@ -0,0 +1,18 @@
+# Instructions
+# You are going to write a program that add to a travel_log,
+# You can see a travel_log which is a List that contain 2 dictionaries.
+
+# Write a function that will work the following line of code on line 21 to add the  entry for Russia to the travel_log.
+
+# add_new_country {"Russia",2,["Moscow","Saint Peterburg"]}
+
+# You 've visited Russia 2 times.
+# You 've  been to Moscow and Saint Peterburg 
+
+# Do Not modify the travel_log directly,You need to create  a functiom that modify it 
+
+# Hint
+# Look at the function call above to see what the nae of function should be.
+# the input for the function are positional arguments.the order is very iportant.
+
+
