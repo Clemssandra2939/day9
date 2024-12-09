1b1fe28df505e361b2b77f77547b3d49cfa4b434
Clemssandra2939 <sclems600@gmail.com>
2024-12-09 05:45:36 +0100

    Added day 7

--- DictionaryinaList.py
+++ DictionaryinaList.py
@@ -4,14 +4,16 @@ travel_log = [
 {
     "country" :"France",
     "total_visited": 12,
-    "cities_visited" : ["Paris","Lille","Dijon"],
-     
+    "cities_visited" : ["Paris","Lille","Dijon"],    
 }, 
 {
     "country" :"Germany",
     "total_visited": 5,
-    "cities_visited" :["Berlin","Hamburg","stuttgart"],
-        
+    "cities_visited" :["Berlin","Hamburg","stuttgart"],        
 },
 ]
-# TODO: Write the function that will allow new country 
+# TODO: Write the function that will allow new country to be added to travel_log
+
+
+add_new_country("Russia",2,["Moscow","Saint Peterburg"])
+print(travel_log)
--- play.py
+++ play.py
@@ -16,3 +16,31 @@
 # the input for the function are positional arguments.the order is very iportant.
 
 
+Function that allows multiple input
+# def greet_with(name,location):
+#     print(f"Hello {name}")
+#     print(f"wat is it like in {location}")
+
+# greet_with("Sandra", "Enugu")
+
+# when the postion of the data is switched like
+# def greet_with(name,location):
+#     print(f"Hello {name}")
+#     print(f"wat is it like in {location}")
+
+# greet_with("Enugu", "Sandra")
+
+# this wat it is goinh to be like
+# ##Hello Enugu
+# Wwat is it like in Sandra
+
+# placing it in the wrong parameters,this is called postional argument
+
+# Keyword arguemnt
+
+# Keyword argument is a way of resolving those kind of errors of poistional argument
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"wat is it like in {location}")
+
+greet_with(location = "Enugu", name = "Sandra")
