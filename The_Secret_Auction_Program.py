fd33d6e4443cbbfd26f99498491c6fec2424db0e
Clemssandra2939 <sclems600@gmail.com>
2024-12-10 05:37:26 +0100

    Created nd working with flowchart

--- The_Secret_Auction_Program.py
+++ The_Secret_Auction_Program.py
@@ -1,3 +1,10 @@
+from replit import clear
 # Hint: You can clear() to clear the otput in the console.abs
 
- 
+from art import logo
+print(logo)
+
+print("Welcome to the Secret Auction Program")
+input("What is your name?/n")
+input("what's yor bid?/n")
+input("Are there any other bidders?Type 'yes' or 'no'./n")
