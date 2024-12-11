a3ec9d9ca33e8be95e624a7385c9eeb25a833a32
Clemssandra2939 <sclems600@gmail.com>
2024-12-11 05:39:42 +0100

    Added a while loops

--- The_Secret_Auction_Program.py
+++ The_Secret_Auction_Program.py
@@ -4,16 +4,30 @@ from replit import clear
 from art import logo
 print(logo)
 
-print("Welcome to the Secret Auction Program")
-name = input("What is your name?/n")
-price = input("what's yor bid? $ /n")
+bids = {}
+bidding_finished = False
 
-info_auction = {
-    "Chima": 120,
-}
+# finding the highest bidder
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount =bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with  bid of  ${highest_bid}")
 
+while not bidding_finished: 
+    print("Welcome to the Secret Auction Program")
+    name = input("What is your name?/n")
+    price = input("what's yor bid? $ /n")
+    bids =  [name] = price 
 answer = input("Are there any other bidders?Type 'yes' or 'no'./n")
-if  answer == "yes":
+if  answer == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+elif answer == "yes":
     clear()
-
+  
     
